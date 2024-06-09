fda14aec287fb64a0561c6928f284775fe2b6514
miguerlfr <luismiguelvargasjaimes@gmail.com>
2024-06-08 20:22:23 -0500

    cambios acualizados

--- database/conexionMongoose.js
+++ database/conexionMongoose.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const dbConexion = async () => {
     try {
-        await mongoose.connect(process.env.CNX_MONGO);
+        await mongoose.connect(process.env.CNX_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log("Conexion bd establecida");
     } catch (error) {
         console.log("Error al conectar la base de datos:", error);
