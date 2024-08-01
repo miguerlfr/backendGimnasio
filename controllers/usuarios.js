9c05714b483cba061a9b344138e315efa19443bf
miguerlfr <luismiguelvargasjaimes@gmail.com>
2024-08-01 14:11:21 -0500

    cambios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -176,8 +176,7 @@ const httpUsuarios = {
 	},
 	putUsuariosContrasena: async (req, res) => {
 		try {
-			const { id } = req.params;
-			const { nuevaContrasenia } = req.body;
+			const { id, nuevaContrasenia } = req.body;
 	
 			// Validar que la nueva contraseña no esté vacía
 			if (!nuevaContrasenia) {
